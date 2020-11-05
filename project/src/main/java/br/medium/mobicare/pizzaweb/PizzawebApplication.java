9a37acff7bec6ba3d1cf89b6fd7267e682c74bb1
Pablo Manoel <pablomanoel@id.uff.br>
2020-11-05 18:42:52 -0300

    Incluindo projeto modelo

---
+++ project/src/main/java/br/medium/mobicare/pizzaweb/PizzawebApplication.java
@@ -0,0 +1,13 @@
+package br.medium.mobicare.pizzaweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzawebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzawebApplication.class, args);
+	}
+
+}
